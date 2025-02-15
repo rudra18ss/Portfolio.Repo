c23092b0f13767b0e601e1989f0e3790f1cf4a33
pratapsinghr674 <pratapsinghr674@gmail.com>
2025-02-15 06:16:35 +0000

    Code Update

---
+++ index.js
@@ -0,0 +1,98 @@
+// turn page when prev or next button is clicked
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')){
+            pageTurn.classList.remove('turn');
+            setTimeout(()=>{
+                pageTurn.style.zIndex = 20 - index;
+            },500);
+        }
+        else{
+            pageTurn.classList.add('turn');
+            setTimeout(()=>{
+                pageTurn.style.zIndex = 20 + index;
+            },500);
+        }
+    }
+})
+
+// contact me button whem click
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:4017967399.
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((page,index) =>{
+        setTimeout(()=>{
+            page.classList.add('turn');
+
+            setTimeout(()=>{
+                page.style.zIndex = 20 + index;
+            },500)
+        },(index + 1) * 200 + 100)
+    })
+}
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex(){
+    pageNumber--;
+    if(pageNumber < 0){
+        pageNumber = totalPages - 1;
+    }
+}
+
+//back profile button
+
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_,index) =>{
+        setTimeout(()=>{
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(()=>{
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            },500)
+        },(index + 1) * 200 + 100)
+})
+}
+
+//opening animation
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
+setTimeout(()=>{
+    coverRight.classList.add('turn');
+},2100)
+
+setTimeout(()=>{
+    coverRight.style.zIndex = -1;
+},2800)
+
+//opening animation (page left pr profile page animation)
+setTimeout(()=>{
+    pageLeft.style.zIndex = 20;
+},3200)
+
+//opening animation all pages
+pages.forEach((_,index) =>{
+    setTimeout(()=>{
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(()=>{
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        },500)
+    },(index + 1) * 200 + 2100)
+})
